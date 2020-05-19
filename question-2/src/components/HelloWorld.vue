a7fe80403889604c2642ff2aa443747f4e15a3dd
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-05-19 18:41:08 +0300

    Implemented Table with data population and pagination

--- question-2/src/components/HelloWorld.vue
+++ question-2/src/components/HelloWorld.vue
@@ -1,40 +1,110 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="example">
+    <button v-on:click="previousPage">Previous Page</button>
+    <button v-on:click="nextPage">Next Page</button>
+
+    <table id="firstTable">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Photos</th>
+          <th>Thumbnail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="album in tableAlbums" :key="album.id">
+          <td>{{album.id}}</td>
+          <td>{{album.title}}</td>
+          <td>
+            <ul class="photos">
+              <li v-for="photo in album.photos" :key="photo">
+                {{photo}}
+              </li>
+            </ul>
+          </td>
+          <img :src="album.thumbnail" />
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+const PAGE_SIZE = 25;
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      albums: [],
+      photos: [],
+      page: 0,
+      tableAlbums: [],
+      loading: false,
+      error: undefined
+    };
+  },
+  mounted: function() {
+    this.loading = true;
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/albums")
+      .then(res => {
+        this.albums = res.data;
+      })
+      .catch(err => {
+        this.loading = false;
+        this.error = err;
+      });
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then(res => {
+        this.photos = res.data;
+        let albums = this.albums;
+        let photos = this.photos;
+
+        this.albums = albums.map(function(album) {
+          let albumPhotos = photos.filter((photo) => album.id  === photo.albumId);
+          album.photos = albumPhotos.map((albumPhoto) => albumPhoto.title).slice(0, 3);
+          album.thumbnail = albumPhotos[0].thumbnailUrl;
+          return (album);
+        });
+
+        this.tableAlbums = this.albums.slice(0, PAGE_SIZE);
+        this.loading = false;
+      })
+      .catch(err => {
+        this.loading = false;
+        this.error = err;
+      });
+  },
+  methods: {
+    nextPage: function () {
+      let nextPage = this.page + 1;
+
+      let startingIndex = PAGE_SIZE * nextPage;
+      let endingIndex = startingIndex + PAGE_SIZE;
+
+      if(startingIndex < this.albums.length) {
+        this.tableAlbums = this.albums.slice(startingIndex, endingIndex);
+        this.page = nextPage;
+      }
+    },
+    previousPage: function() {
+      let previousPage = this.page - 1;
+
+      if(previousPage >= 0) {
+        let startingIndex = PAGE_SIZE * previousPage;
+        let endingIndex = startingIndex + PAGE_SIZE;
+        this.tableAlbums = this.albums.slice(startingIndex, endingIndex);
+        this.page = previousPage;
+      }
+    }
   }
 }
 </script>
